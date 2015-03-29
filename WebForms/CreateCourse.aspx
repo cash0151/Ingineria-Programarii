f4e75682f0aa67343247d6b788bd5ba8be88dd4f
cash0151 <cash0151@yahoo.com>
2015-03-29 18:14:44 +0300

    Pagina creare curs versiune initiala

--- WebForms/CreateCourse.aspx
+++ WebForms/CreateCourse.aspx
@@ -1,7 +1,33 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="CreateCourse.aspx.cs" Inherits="WebForms_CreateCourse" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        #TextArea1 {
+            height: 87px;
+            width: 367px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <p>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </p>
+    <p>
+        Nume Curs:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" DataTextField="NumeCategorie" DataValueField="Id">
+        </asp:DropDownList>
+        
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Categorii_Cursuri]"></asp:SqlDataSource>
+        
+    </p>
+    <p>
+        Continut</p>
+    <p>
+        <asp:TextBox id="TextBox2" TextMode="multiline" Columns="50" Rows="5" runat="server" /></p>
+    <p>
+        <asp:Button ID="Button4" runat="server" Text="Creaza" />
+    </p>
 </asp:Content>
 
--- WebForms/CreateCourse.aspx.cs
+++ WebForms/CreateCourse.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +11,49 @@ public partial class WebForms_CreateCourse : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Label1.Text = "";
+        if (DropDownList1.SelectedIndex < 0)
+        {
+            Label1.Text = "Nu ai selectat categorie";
+            return;
+        }
+        if (Session["login"] != null)
+        {
+
+            String nume_utilizator=((AppData) Session["login"]).Utilizator;
+            SqlConnection conn;
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            //Crearea unui query
+            conn.Open();
+            SqlCommand c = new SqlCommand("SELECT id FROM useri WHERE nume=@nume AND tip='profesor'", conn);
+            c.Parameters.Add(new SqlParameter("@nume", TypeCode.String));
+            c.Parameters["@nume"].Value = nume_utilizator;
+            object o = (object)c.ExecuteScalar();
+            int idProfesor = (int)o;
+            if (idProfesor == null)
+            {
+                Label1.Text = "Nu am putut crea cursul";
+                return;
+            } 
+
+
+            c = new SqlCommand("INSERT INTO Cursuri(numeCurs,Profesor,Continut,Categorie) VALUES(@NumeCurs,@idProfesor,@Continut,@idCategorie);", conn);
+            c.Parameters.Add(new SqlParameter("@NumeCurs", TypeCode.String));
+            c.Parameters["@NumeCurs"].Value = TextBox1.Text;
+            c.Parameters.Add(new SqlParameter("@idProfesor", TypeCode.Int32));
+            c.Parameters["@idProfesor"].Value = idProfesor;
+            c.Parameters.Add(new SqlParameter("@Continut", TypeCode.String));
+            c.Parameters["@Continut"].Value = TextBox2.Text;
+            c.Parameters.Add(new SqlParameter("@idCategorie", TypeCode.Int32));
+            c.Parameters["@idCategorie"].Value = DropDownList1.SelectedItem.Value;
+            c.ExecuteNonQuery();
+            conn.Close();
+        }
+        else
+        {
+            Label1.Text = "Nu esti logat";
+        }
 
     }
+
 }
